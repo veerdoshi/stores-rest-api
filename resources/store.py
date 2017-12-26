bbcac3a8f68bb5e3e4e4169ad076d1d0898026a1
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-26 10:02:13 -0800

    Update store.py

--- resources/store.py
+++ resources/store.py
@@ -29,4 +29,4 @@ class Store(Resource):
 
 class StoreList(Resource):
     def get(self):
-        return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
