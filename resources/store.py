3b34f3b36991ad54cf57d674b0e42a93dc02cd59
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-25 18:41:56 -0800

    Update store.py

--- resources/store.py
+++ resources/store.py
@@ -7,15 +7,15 @@ class Store(Resource):
         store = StoreModel.find_by_name(name)
         if store:
             return store.json()
-        return {'message': 'Store not found'}, 404
+        return {'message': 'Sensor not found'}, 404
     def post(self, name):
         if StoreModel.find_by_name(name):
-            return {'message': "A store with name '{}' already exists.".format(name)}, 400
+            return {'message': "A sensor with name '{}' already exists.".format(name)}, 400
         store = StoreModel(name)
         try:
             store.save_to_db()
         except:
-            return {'message': 'An error occurred while creating the store.'}, 500
+            return {'message': 'An error occurred while creating the sensor.'}, 500
 
         return store.json(), 201
 
@@ -23,9 +23,8 @@ class Store(Resource):
         store = StoreModel.find_by_name(name)
         if store:
             store.delete_from_db()
-
-            return {'message': 'Store deleted'}
-
+            return {'message': 'Sensor deleted'}
+        return {'message': 'Sensor not found'}, 404
 class StoreList(Resource):
     def get(self):
-        return {'stores': [store.json() for store in StoreModel.query.all()]}
+        return {'sensors': [store.json() for store in StoreModel.query.all()]}
