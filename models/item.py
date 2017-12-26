9deebf36343a2937b18dce87304522728549096c
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-25 18:51:13 -0800

    Update item.py

--- models/item.py
+++ models/item.py
@@ -15,7 +15,7 @@ class ItemModel(db.Model):
         self.price = price
         self.store_id = store_id
     def json(self):
-        return {'name': self.name, 'magnitude': self.price}
+        return {'name': self.name, 'price': self.price}
 
     @classmethod
     def find_by_name(cls, name):
