65bed63d8920fbc0f92f71f52c2d822f35808841
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-25 19:21:39 -0800

    Update item.py

--- models/item.py
+++ models/item.py
@@ -14,6 +14,7 @@ class ItemModel(db.Model):
         self.name = name
         self.price = price
         self.store_id = store_id
+
     def json(self):
         return {'name': self.name, 'price': self.price}
 
