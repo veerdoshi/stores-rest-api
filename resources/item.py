e9a0b182cf5fac7349e0a6b31c2f0e01d9942515
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-25 19:13:09 -0800

    Update item.py

--- resources/item.py
+++ resources/item.py
@@ -30,7 +30,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, **data)
+        item = ItemModel(name, data['price'], data['store_id'])
    #     try:
         item.save_to_db()
  #       except:
