e7b7ae38d866eccba70497e6830b6277cef14be2
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-25 19:23:16 -0800

    Update store.py

--- resources/store.py
+++ resources/store.py
@@ -1,6 +1,5 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models.store import StoreModel
-
 
 class Store(Resource):
     def get(self, name):
@@ -8,14 +7,16 @@ class Store(Resource):
         if store:
             return store.json()
         return {'message': 'Store not found'}, 404
+
     def post(self, name):
         if StoreModel.find_by_name(name):
             return {'message': "A store with name '{}' already exists.".format(name)}, 400
+
         store = StoreModel(name)
         try:
             store.save_to_db()
         except:
-            return {'message': 'An error occurred while creating the store.'}, 500
+            return {"message": "An error occurred creating the store."}, 500
 
         return store.json(), 201
 
@@ -23,8 +24,9 @@ class Store(Resource):
         store = StoreModel.find_by_name(name)
         if store:
             store.delete_from_db()
-            return {'message': 'Store deleted'}
-        return {'message': 'Store not found'}, 404
+
+        return {'message': 'Store deleted'}
+
 class StoreList(Resource):
     def get(self):
-        return {'stores': [store.json() for store in StoreModel.query.all()]}
+        return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
