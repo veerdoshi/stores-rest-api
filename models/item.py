6868a2305f0222e9cb58e12156f5a019ba1a6fbc
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-26 10:27:38 -0800

    Update item.py

--- models/item.py
+++ models/item.py
@@ -3,7 +3,7 @@ from db import db
 class ItemModel(db.Model):
     __tablename__ = 'items'
 
-    id = db.Column(db.Integer())
+    id = db.Column(db.Integer(primary_key=True))
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
